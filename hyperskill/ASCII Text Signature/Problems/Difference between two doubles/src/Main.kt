3f2e0c798ce83218393f01b6b4be1a0f67284503
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-13 13:10:34 -0300

    Difference between two doubles.

---
+++ hyperskill/ASCII Text Signature/Problems/Difference between two doubles/src/Main.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val d1 = scanner.nextDouble()
+    val d2 = scanner.nextDouble()
+
+    val result = d2 - d1
+
+    println(result)
+}
