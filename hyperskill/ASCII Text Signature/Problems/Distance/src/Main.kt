5bc380c7c052058858c03108d4555bf955df4f1c
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-13 13:08:32 -0300

    Distance.

---
+++ hyperskill/ASCII Text Signature/Problems/Distance/src/Main.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val s = scanner.nextDouble()
+    val d = scanner.nextDouble()
+
+    val result = s / d
+
+    println(result)
+}
