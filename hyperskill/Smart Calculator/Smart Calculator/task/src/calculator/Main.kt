b668b4a4436e5bfac0f16262ea2455db9362ce09
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-28 18:31:20 -0300

    Stage 1/8: 2+2.

---
+++ hyperskill/Smart Calculator/Smart Calculator/task/src/calculator/Main.kt
@@ -0,0 +1,13 @@
+package calculator
+
+import java.util.*
+
+fun main() {
+    println("Hello, World!")
+    val scanner = Scanner(System.`in`)
+
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
+
+    println(a + b)
+}
