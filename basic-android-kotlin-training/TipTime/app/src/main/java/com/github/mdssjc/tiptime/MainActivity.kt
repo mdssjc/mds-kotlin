c2e7be58f00b167eea24acf69aefb5349add2776
Marcelo dos Santos <mdssjc@gmail.com>
2021-05-02 22:30:55 -0300

    Create a more polished user experience.

--- basic-android-kotlin-training/TipTime/app/src/main/java/com/github/mdssjc/tiptime/MainActivity.kt
+++ basic-android-kotlin-training/TipTime/app/src/main/java/com/github/mdssjc/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.github.mdssjc.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mdssjc.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -17,10 +21,14 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener {
             calculateTip()
         }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null) {
             displayTip(0.0)
@@ -44,5 +52,15 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
