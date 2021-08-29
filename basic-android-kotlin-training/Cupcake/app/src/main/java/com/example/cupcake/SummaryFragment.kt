de7f8fba834de8f8109eba4087d7dd313ef7c3b5
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-28 22:24:00 -0300

    feat: add Shared ViewModel Across Fragments

--- basic-android-kotlin-training/Cupcake/app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ basic-android-kotlin-training/Cupcake/app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.cupcake.databinding.FragmentSummaryBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
  * [SummaryFragment] contains a summary of the order details with a button to share the order
@@ -14,10 +16,8 @@ import com.example.cupcake.databinding.FragmentSummaryBinding
  */
 class SummaryFragment : Fragment() {
 
-    // Binding object instance corresponding to the fragment_summary.xml layout
-    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
-    // when the view hierarchy is attached to the fragment.
     private var binding: FragmentSummaryBinding? = null
+    private val sharedViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,7 +32,9 @@ class SummaryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding?.apply {
-            sendButton.setOnClickListener { sendOrder() }
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
+            summaryFragment = this@SummaryFragment
         }
     }
 
