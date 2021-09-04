c2ec6cc5446b299f6c2fda670a20393f560ef635
Marcelo dos Santos <mdssjc@gmail.com>
2021-09-04 15:12:59 -0300

    feat: add Load and display images from the Internet

---
+++ basic-android-kotlin-training/MarsPhotos/app/src/main/java/com/example/android/marsphotos/overview/PhotoGridAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.android.marsphotos.overview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.marsphotos.databinding.GridViewItemBinding
+import com.example.android.marsphotos.network.MarsPhoto
+
+/**
+ * This class implements a [RecyclerView] [ListAdapter] which uses Data Binding to present [List]
+ * data, including computing diffs between lists.
+ */
+class PhotoGridAdapter : ListAdapter<MarsPhoto,
+        PhotoGridAdapter.MarsPhotoViewHolder>(DiffCallback) {
+
+    /**
+     * Create new [RecyclerView] item views (invoked by the layout manager)
+     */
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): PhotoGridAdapter.MarsPhotoViewHolder {
+        return MarsPhotoViewHolder(GridViewItemBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    /**
+     * Replaces the contents of a view (invoked by the layout manager)
+     */
+    override fun onBindViewHolder(holder: PhotoGridAdapter.MarsPhotoViewHolder, position: Int) {
+        val marsPhoto = getItem(position)
+        holder.bind(marsPhoto)
+    }
+
+    /**
+     * The MarsPhotosViewHolder constructor takes the binding variable from the associated
+     * GridViewItem, which nicely gives it access to the full [MarsPhoto] information.
+     */
+    class MarsPhotoViewHolder(private var binding: GridViewItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(MarsPhoto: MarsPhoto) {
+            binding.photo = MarsPhoto
+            binding.executePendingBindings()
+        }
+    }
+
+    /**
+     * Allows the RecyclerView to determine which items have changed when the [List] of
+     * [MarsPhoto] has been updated.
+     */
+    companion object DiffCallback : DiffUtil.ItemCallback<MarsPhoto>() {
+        override fun areItemsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
+            return oldItem.imgSrcUrl == newItem.imgSrcUrl
+        }
+    }
+}
