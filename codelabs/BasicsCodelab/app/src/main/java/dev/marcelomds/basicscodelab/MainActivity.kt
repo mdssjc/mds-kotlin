b04bff793934d5fdc18f22d642aaf445965cde7f
Marcelo dos Santos <mdssjc@gmail.com>
2023-08-05 14:57:12 -0300

    feat(wip): add Jetpack Compose basics - part 1

---
+++ codelabs/BasicsCodelab/app/src/main/java/dev/marcelomds/basicscodelab/MainActivity.kt
@@ -0,0 +1,54 @@
+package dev.marcelomds.basicscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.marcelomds.basicscodelab.ui.theme.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(24.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "Text preview")
+@Composable
+fun GreetingPreview() {
+    BasicsCodelabTheme {
+        MyApp()
+    }
+}
